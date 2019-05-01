9910ca24bf3355a3f2bc16dfab61100e5ca0c40c
Ryan Schlesinger <ryan@ryanschlesinger.com>
2019-05-01 09:01:15 -0700

    No more shallow clones

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/ryansch/yadr.git "$HOME/.yadr"
+    git clone https://github.com/ryansch/yadr.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     git checkout ryansch-changes
     [ "$1" = "ask" ] && export ASK="true"
